d4ca064827d75d2654787261bd6c9d4726a60c00
Danny August Ramaputra <daystram@gmail.com>
2024-09-01 18:19:36 +0900

    change: make heartbeat and rotary send

--- src/util.rs
+++ src/util.rs
@@ -6,42 +6,43 @@ use rtic_monotonics::rp2040::prelude::*;
 
 use crate::kb::Mono;
 
-pub async fn halt_blink<P: OutputPin + ?Sized>(led: &mut P, us: u32) -> Result<(), P::Error> {
-    match led.set_high() {
+pub async fn blink<P: OutputPin>(
+    pin: &mut P,
+    duty: u8,
+    delay: <Mono as Monotonic>::Duration,
+) -> Result<(), P::Error> {
+    match pin.set_high() {
         Ok(_) => (),
         Err(e) => return Err(e),
     };
-    halt((us / 2) as u64).await;
-    match led.set_low() {
+    Mono::delay((delay.to_nanos() * duty as u64 / u8::MAX as u64).nanos()).await;
+    match pin.set_low() {
         Ok(_) => (),
         Err(e) => return Err(e),
     };
-    halt((us / 2) as u64).await;
+    Mono::delay((delay.to_nanos() * (u8::MAX - duty) as u64 / u8::MAX as u64).nanos()).await;
     Ok(())
 }
 
-pub async fn halt(us: u64) {
-    Mono::delay(us.micros()).await;
-}
-
 pub async fn lerp(
-    pin: &mut Box<dyn SetDutyCycle<Error = gpio::Error>>,
+    pin: &mut Box<dyn SetDutyCycle<Error = gpio::Error> + Sync + Send>,
     from: u16,
     to: u16,
     step: u16,
-    delay_ms: u64,
+    delay: <Mono as Monotonic>::Duration,
 ) {
     let diff = if from < to {
         (to - from) / step
     } else {
         (from - to) / step
     };
+    let step_delay = (delay.to_nanos() / step as u64).nanos();
 
     for d in (0..step)
         .map(|x| x * diff)
         .map(|x| if from < to { from + x } else { from - x })
     {
-        Mono::delay(delay_ms.millis()).await;
         pin.set_duty_cycle(d).unwrap();
+        Mono::delay(step_delay).await;
     }
 }
